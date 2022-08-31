6cf3667c702fa9dbddecb5e41f678aee143e4456
a rahni <rahniahmed@gmail.com>
2022-08-31 18:31:45 +0200

    first commit -init project

---
+++ src/main/java/fr/m2i/apitchat/ApitchatApplication.java
@@ -0,0 +1,13 @@
+package fr.m2i.apitchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/m2i/apitchat/ApitchatApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.apitchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitchatApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
