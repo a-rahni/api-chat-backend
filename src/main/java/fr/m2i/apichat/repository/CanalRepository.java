acf142eaa407372dc775e40a929d7f93e7adb2db
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-05 12:02:05 +0200

    Test DB Room et changement pom.xml

---
+++ src/main/java/fr/m2i/apichat/repository/CanalRepository.java
@@ -0,0 +1,10 @@
+package fr.m2i.apichat.repository;
+
+import fr.m2i.apichat.model.Canal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public  interface CanalRepository extends JpaRepository<Canal,Long> {
+    Canal findCanalByName(String roomName);
+}
