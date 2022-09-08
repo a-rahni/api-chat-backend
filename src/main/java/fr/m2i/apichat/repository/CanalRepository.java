f01ff39aa68e74e468b2a0dbdbb630909d6f5873
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-08 08:17:51 +0200

    Crétion de repo Canal

--- src/main/java/fr/m2i/apichat/repository/CanalRepository.java
+++ src/main/java/fr/m2i/apichat/repository/CanalRepository.java
@@ -4,7 +4,12 @@ import fr.m2i.apichat.model.Canal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public  interface CanalRepository extends JpaRepository<Canal,Long> {
-    Canal findByName(String name);
+
+    List<Canal> findByNameContaining(String name);
+
 }
