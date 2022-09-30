f26630cbfd4e5c0996828d2d26537c90160384e1
Christian RAKOTO <crakoto@yahoo.fr>
2022-09-30 11:25:36 +0200

    L'autre methode pour injecter avec stream

---
+++ src/test/resources/insert-canaux.sql
@@ -0,0 +1,4 @@
+INSERT INTO canaux (name, created_at, `description`, updated_at)
+VALUES ('General', NOW(), 'Desc canal General', NOW());
+INSERT INTO canaux (name, created_at, `description`, updated_at)
+VALUES ('Room 1', NOW(), ' Desc Room 1', NOW());
